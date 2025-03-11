c3be4fb6c3e57960746a6ff9b7c433854f1ca928
dhavalpotdar <dhavalpotdar@gmail.com>
2025-03-11 14:31:34 -0400

    changed image path to /headshot.jpg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
     >
       <motion.img
         variants={itemVariants}
-        src="./public/headshot.jpg"
+        src="./headshot.jpg"
         alt="Dhaval Potdar"
         className="w-32 h-32 sm:w-40 sm:h-40 rounded-full border-4 border-gray-700 shadow-lg"
       />
